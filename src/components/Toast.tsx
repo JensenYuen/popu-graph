93ff8ab54e09d5e9919612e58bf0748520d6a4d1
jensen yuen <jensenyuen@outlook.com>
2023-01-15 16:31:44 +0900

    toast-reactify追加

---
+++ src/components/Toast.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { toast, ToastContainer } from 'react-toastify';
+
+interface props {
+  message: string;
+}
+
+const Toast = ({ message }: props) => {
+  toast(message,{
+    position: toast.POSITION.TOP_RIGHT,
+    autoClose: false
+  })
+
+  return (
+    <ToastContainer autoClose={3000} />
+  )
+}
+
+export default Toast;
